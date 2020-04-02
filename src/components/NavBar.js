75cd9febaa7b2c250ffea767ca388aba0a485b82
Zac <szhu76@gmail.com>
2020-04-02 21:06:48 +1100

    initial-test: *WIP code cleaning

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => (
     <nav className="navbar navbar-light bg-light text-center">
-        <a className="navbar-brand" href="#">Aesop</a>
+        <a className="navbar-brand" href="/#">Aesop</a>
     </nav>
 )
 
