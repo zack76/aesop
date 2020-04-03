d860ef6ba0041b9a45d5951a9b7230b3feeff12f
Zac <szhu76@gmail.com>
2020-04-03 23:23:45 +1100

    UI-with-test-coverage: UI with api & test coverage

--- src/service/ItemService.js
+++ src/service/ItemService.js
@@ -2,7 +2,8 @@ import BaseService from './BaseService';
 import { Config } from '../config';
 
 class ItemService {
-    getItemDetail = ()=> BaseService.get(Config.CROS_ANYWHERE + Config.AESOP_URL);
+    // getItemDetail = ()=> BaseService.get(Config.CROS_ANYWHERE + Config.AESOP_URL);
+    getItemDetail = ()=> BaseService.get(Config.AESOP_URL);
 }
 const itemService = new ItemService();
 export default itemService;
