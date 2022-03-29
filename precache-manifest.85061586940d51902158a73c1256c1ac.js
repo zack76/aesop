836044c4526647e1a49adc133cdc4d6182e0b6f0
Zack Zhu <51350376+zack76@users.noreply.github.com>
2022-03-29 22:52:12 +1100

    Updates

---
+++ precache-manifest.85061586940d51902158a73c1256c1ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c1c6cbfe2bcf60e159c0c41a14285d",
+    "url": "/test-code/index.html"
+  },
+  {
+    "revision": "a39ed00a107570b42aa3",
+    "url": "/test-code/static/css/2.1dbd2d06.chunk.css"
+  },
+  {
+    "revision": "8ef7a2e2fc0da72ebb2f",
+    "url": "/test-code/static/css/main.95d3ca67.chunk.css"
+  },
+  {
+    "revision": "a39ed00a107570b42aa3",
+    "url": "/test-code/static/js/2.3af8127e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test-code/static/js/2.3af8127e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef7a2e2fc0da72ebb2f",
+    "url": "/test-code/static/js/main.523db52c.chunk.js"
+  },
+  {
+    "revision": "c4ac83318922bf7834b5",
+    "url": "/test-code/static/js/runtime-main.32952d68.js"
+  }
+]);
